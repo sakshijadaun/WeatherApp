ab670b99e59bddf85818ffcc4627caa1fe3f51d3
Sakshi Jadaun <sakshijadaun080@gmail.com>
2023-12-30 23:32:15 +0530

    initial commit

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 console.log("Hello");
+
+const API_KEY = "7c328f2c39ed0b62abe8ef94436c4d5a";
+
+async function showWeather(){
+    // let latitude = 15.3333;
+    // let longitude = 74.0833;
+    let city = "hathras";
+
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`)
+    const data = await response.json();
+    
+    console.log("Weather data: =>" + data);
+
+    let newPara = document.createElement('p');
+    newPara.textContent = `${data?.main?.temp.toFixed(2)} *c`;
+    document.body.appendChild(newPara);
+    
+}
