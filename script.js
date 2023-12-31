6faf11c632e42698fab648a9989abbf132483abd
Sakshi Jadaun <sakshijadaun080@gmail.com>
2023-12-31 18:35:52 +0530

    start adding JS

--- script.js
+++ script.js
@@ -1,19 +1,67 @@
-console.log("Hello");
 
-const API_KEY = "7c328f2c39ed0b62abe8ef94436c4d5a";
 
-async function showWeather(){
-    // let latitude = 15.3333;
-    // let longitude = 74.0833;
-    let city = "hathras";
 
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`)
-    const data = await response.json();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log("Hello");
+
+// const API_KEY = "7c328f2c39ed0b62abe8ef94436c4d5a";
+
+// function renderWeatherInfo(data){
+//     let newPara = document.createElement('p');
+//     newPara.textContent = `${data?.main?.temp.toFixed(2)} *c`;
+//     document.body.appendChild(newPara);
+
+// }
+// async function showWeather(){
+  
+//     try{
+//         let city = "london";
+
+//         const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`)
+//         const data = await response.json();
+        
+//         console.log("Weather data: =>" , data);
+        
+//         renderWeatherInfo(data);
+//     }catch(err){
+//         console.log("Error Found" , err);
+//     }
+  
     
-    console.log("Weather data: =>" + data);
-
-    let newPara = document.createElement('p');
-    newPara.textContent = `${data?.main?.temp.toFixed(2)} *c`;
-    document.body.appendChild(newPara);
-    
-}
+// }
