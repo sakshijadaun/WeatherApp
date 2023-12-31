c84ffa71f31809ada5896487dda2cf6f1da6477d
Sakshi Jadaun <sakshijadaun080@gmail.com>
2023-12-31 19:38:55 +0530

    switchtab function add

--- script.js
+++ script.js
@@ -1,6 +1,47 @@
+const userTab = document.querySelector("[data-userWeather]");
+const searchTab = document.querySelector("[data-searchWeather]");
+const userContainer = document.querySelector(".weather-container");
+
+const grantAccessContainer = document.querySelector(".grant-location-container");
+const searchForm = document.querySelector("[data-searchForm]");
+const loadingScreen = document.querySelector(".loading-container");
+const userInfoContainer = document.querySelector("user-info-cotainer");
+
+//Intially variables needed
+let oldTab = userTab;
+const API_Key = "";
+old.classList.add("current-Tab");
+
+//function to switch between tabs
+function switchTab(newTab){
+    if(newTab != oldTab){ //checks that we are on usertab or searchtab
+        oldTab.classList.remove("current-tab");
+        oldTab = newTab;
+        oldTab.classList.add("current-tab");
+        
+        if(!searchForm.classList.contains("active")){  //checks searchform invisible than make it visible
+            userInfoContainer.classList.remove("active");
+            grantAccessContainer.classList.remove("active");
+            searchForm.classList.add("active");
+        }else{  //if visible then make it invisble and and make a function to your weather tab
+                //and check local storage first for coordinates,if we haves saved them there
+            searchForm.classList.remove("active");
+            userInfoContainer.classList.remove("active");
+            getFromSessionStorage();
+        }
+    } 
 
 
+}
+//this addevent listener will work when click on userTab and call switchTab function
+userTab.addEventListener("click",() => {
+    switchTab(userTab);
+})
 
+//this addevent listener will work when click on searchtab and call switchTab function
+searchTab.addEventListener("click", () => {
+    switchTab(searchTab);
+})
 
 
 
